5e37ba6ce57c14793d5bb5545433d0c654c516a9
elkabrito <alberto8abernal@gmail.com>
2022-01-06 16:16:07 -0600

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,20 @@
+const { io } = require('../index');
+
+// Mensajes de sockets
+io.on('connection', client => {
+
+    console.log('cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('cliente desconectado');
+    });
+
+
+    client.on('mensaje', (payload) => {
+        console.log('mensaje', payload);
+
+        io.emit('mensaje', { admin: 'Nuevo mensaje' });
+
+    });
+
+});
